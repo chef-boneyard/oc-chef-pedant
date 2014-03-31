79b7e0b3d22c7f3d49ea545fb8dadba257756b7a
tylercloke <tyler@opscode.com>
2014-03-31 14:52:11 -0700

    Responded to PR comments and added PUT /org/:id spec.

--- spec/api/organization_spec.rb
+++ spec/api/organization_spec.rb
@@ -9,24 +9,63 @@ require 'json'
 
 describe "/organizations", :organizations do
   describe "GET /organizations" do
-    let(:request_method) { :GET }
     let(:request_url)    { "#{platform.server}/organizations" }
     let(:requestor)      { superuser }
 
-    let(:expected_response) { ok_response }
+    context "when the user requests a list of organizations" do
+      it "should return a valid list of organizations" do
+        get(request_url, requestor).should look_like(
+          # Would actually probably contain additional orgs in body, but this is the only
+          # one we know is there.
+          :body => {
+            platform.test_org.name => "#{platform.server}/organizations/#{platform.test_org.name}"
+          },
+          :status => 200
+        )
+      end
+    end
 
-    should_respond_with 200
   end
 
-
   describe "GET /organizations/:id" do
-    let(:request_method) { :GET }
     let(:request_url)    { "#{platform.server}/organizations/#{platform.test_org.name}" }
     let(:requestor)      { superuser }
 
-    let(:expected_response) { ok_response }
+    context "when the user requests a valid organization" do
 
-    should_respond_with 200
+      it "should return a valid organization object" do
+        get(request_url, requestor).should look_like(
+          # Would actually probably contain additional orgs in body, but this is the only
+          # one we know is there.
+          :body => {
+            "name"                     => platform.test_org.name,
+            "full_name"                => platform.test_org.name,
+            "org_type"                 => "Business",
+            "clientname"               => "#{platform.test_org.name}-validator",
+            "chargify_subscription_id" => nil,
+            "chargify_customer_id"     => nil,
+            "billing_plan"             => "platform-free",
+          },
+          :status => 200
+        )
+
+      end
+
+      it "should return a organization object that contains a valid guid" do
+        parsed_response = JSON.parse(get(request_url, requestor))
+        parsed_response["guid"].should have(32).characters
+      end
+
+      it "should return a valid assigned_at field of the format YYYY/MM/DD HH:MM:SS +TTTT" do
+        parsed_response = JSON.parse(get(request_url, requestor))
+        parts = parsed_response["assigned_at"].split
+
+        /^\d{4}[-\/]\d{1,2}[-\/]\d{1,2}$/.should match(parts[0])
+        /^(\d{2}):(\d{2}):(\d{2})$/.should match(parts[1])
+        /^\+\d\d\d\d$/.should match(parts[2])
+      end
+    end
+
   end
 
   describe "POST /organizations"  do
@@ -39,28 +78,103 @@ describe "/organizations", :organizations do
       }
     end
 
+    context "when the user posts a new organization with a valid body and name" do
+
+      after :each do
+        delete("#{platform.server}/organizations/#{orgname}", superuser)
+      end
+
+      it "should respond with a valid newly created organization" do
+        post("#{platform.server}/organizations", superuser, :payload => request_body).should look_like(
+          :body => {
+            "clientname" => "#{orgname}-validator",
+            "uri" => "#{platform.server}/organizations/#{orgname}"
+          },
+          :status => 201
+        )
+      end
+
+      it "should respond with data containing a valid private key" do
+        result = JSON.parse(post("#{platform.server}/organizations", superuser, :payload => request_body))
+        /-----BEGIN RSA PRIVATE KEY-----/.should match(result["private_key"])
+      end
+    end
+
+  end
+
+  describe "PUT /organizations/:id" do
+    let(:orgname) { "test-#{Time.now.to_i}-#{Process.pid}" }
+    let(:post_request_body) do
+      {
+        full_name: "fullname-#{orgname}",
+        name: orgname,
+        org_type: "Business"
+      }
+    end
+
+    before :each do
+      post("#{platform.server}/organizations", superuser, :payload => post_request_body)
+    end
+
     after :each do
       delete("#{platform.server}/organizations/#{orgname}", superuser)
     end
 
-    it "should respond with a valid newly created organization" do
-      post("#{platform.server}/organizations", superuser, :payload => request_body).should look_like(:body => {
-        "clientname" => "#{orgname}-validator"
-      })
+    context "when the user updates organization object fields with valid info" do
+      let(:new_orgname) { "test-#{Time.now.to_i*3}-#{Process.pid}" }
+
+      after :each do
+        delete("#{platform.server}/organizations/#{new_orgname}", superuser)
+      end
+
+      it "should update the organization object" do
+        # TODO: we don't validate org_type at all
+        payload = {
+          name: new_orgname,
+          org_type: "Pleasure",
+          full_name: new_orgname
+        }
+        put("#{platform.server}/organizations/#{orgname}", superuser, :payload => payload).should look_like(
+          :status => 200,
+          :body => {"uri" => "#{platform.server}/organizations/#{new_orgname}"}
+        )
+
+        get("#{platform.server}/organizations/#{new_orgname}", superuser).should look_like(
+          :status => 200,
+          :body => {
+            "name" => new_orgname,
+            "full_name" => new_orgname,
+            "org_type" => "Pleasure"
+          }
+        )
+      end
     end
 
-    it "should respond with data containing a valid private key" do
-      result = JSON.parse(post("#{platform.server}/organizations", superuser, :payload => request_body))
-      /-----BEGIN RSA PRIVATE KEY-----/.should match(result["private_key"])
+    context "when the user tries to post a private_key" do
+      it "throws an error related to no longer supporting PUT for key updating" do
+        request = put("#{platform.server}/organizations/#{orgname}", superuser,
+                      :payload => {private_key: "some_unused_key"})
+
+        request.should look_like(
+         :status => 410
+        )
+
+        JSON.parse(request).should have_key("error")
+      end
     end
+
   end
 
   ##########################
   # Internal account tests #
   ##########################
 
-  describe "POST /internal-organizations", :'internal-account' do
-    let(:orgname)      { "test-#{Time.now.to_i}-#{Process.pid}" }
+  describe "POST /internal-organizations", :'internal-account', :pending => true do
+    pending("TODO: /internal-organizations tests currently state behind" \
+            "since there is no InternalOrganizations delete endpoint," \
+            "it should probably only be run in dev-vm")
+
+    let(:orgname)      { "precreated-#{Time.now.to_i}-#{Process.pid}" }
     let(:request_body) do
       {
         full_name: "Pre-created",
@@ -75,25 +189,40 @@ describe "/organizations", :organizations do
 
     context "when creating a new org" do
       it "should respond with a valid newly created organization" do
-        authenticated_request(:POST, "#{platform.internal_account_url}/internal-organizations", superuser, :payload => request_body).should look_like(:body => {
-          "clientname" => "#{orgname}-validator"
-        })
+        authenticated_request(:POST, "#{platform.internal_account_url}/internal-organizations", superuser,
+          :payload => request_body).should look_like(
+            :body => {
+              "clientname" => "#{orgname}-validator",
+              "uri" => "#{platform.internal_account_url}/internal-organizations/orgname"
+            },
+            :status => 201
+        )
       end
     end
 
     context "when attempting to create a new org and that org already exists" do
-      it "should respond with a valid newly created organization" do
+      it "should respond with a conflict" do
         # seed the org
         authenticated_request(:POST, "#{platform.internal_account_url}/internal-organizations", superuser, :payload => request_body)
 
-        authenticated_request(:POST, "#{platform.internal_account_url}/internal-organizations", superuser, :payload => request_body).should look_like(:status => 409)
+        authenticated_request(:POST, "#{platform.internal_account_url}/internal-organizations", superuser,
+          :payload => request_body).should look_like(
+            :status => 409,
+            :body => {
+              "error" => "Organization already exists."
+            }
+        )
       end
     end
 
   end
 
-  describe "PUT /internal-organizations", :'internal-account' do
-    let(:orgname) { "test-#{Time.now.to_i}-#{Process.pid}" }
+  describe "PUT /internal-organizations", :'internal-account', :pending => true do
+    pending("TODO: /internal-organizations tests currently state behind" \
+            "since there is no InternalOrganizations delete endpoint," \
+            "it should probably only be run in dev-vm")
+
+    let(:orgname) { "precreated-#{Time.now.to_i}-#{Process.pid}" }
     let(:post_request_body) do
       {
         full_name: "Pre-created",
@@ -120,11 +249,19 @@ describe "/organizations", :organizations do
       it "should update the organization's unassigned field" do
         # since there is no way of actually getting the assigned field back from the API that I know of
         # best tests I can think of
-        authenticated_request(:PUT, "#{platform.internal_account_url}/internal-organizations/#{orgname}", superuser, :payload => put_request_body).should look_like(:status => 200)
+        authenticated_request(:PUT, "#{platform.internal_account_url}/internal-organizations/#{orgname}",
+          superuser, :payload => put_request_body).should look_like(
+            :status => 200,
+            :body => {
+              "uri" => "#{platform.internal_account_url}/internal-organizations/#{orgname}"
+            }
+        )
 
-        get("#{platform.server}/organizations/ponyville", superuser).should look_like(:body => {
-         "assigned_at "=> nil
-        })
+        get("#{platform.server}/organizations/ponyville", superuser).should look_like(
+          :body => {
+            "assigned_at "=> nil
+          }
+        )
       end
     end
 
@@ -139,9 +276,14 @@ describe "/organizations", :organizations do
         }
       end
 
-      it "should return an error" do
-        authenticated_request(:PUT, "#{platform.internal_account_url}/internal-organizations/#{orgname}", superuser, :payload => put_request_body).should look_like(:body => {                                                                  "error" => "Cannot assign org #{orgname} - unassigned=true is only allowable operation",
-        })
+      it "should return a bad request error" do
+        authenticated_request(:PUT, "#{platform.internal_account_url}/internal-organizations/#{orgname}",
+          superuser, :payload => put_request_body).should look_like(
+            :body => {
+              "error" => "Cannot assign org #{orgname} - unassigned=true is only allowable operation",
+            },
+            :status => 400
+        )
       end
     end
   end
